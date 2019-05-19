054f363922eb0582e1649f480fe06ae8a1848093
Ryan Hoffmann <ryan.hoffmann@live.com>
2019-05-19 17:56:54 -0500

    remove unneeded files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ryan Hoffmann`,
+    description: `Ryan Hoffmann's CV`,
+    author: `@rh_hoff`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
